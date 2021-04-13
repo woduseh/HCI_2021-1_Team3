33b947d518d4e604080b317c6b257d0d806da72b
woduseh <39576391+woduseh@users.noreply.github.com>
2021-04-13 19:17:31 +0900

    모션인식 기능 추가
    학교정문 씬만 업데이트 완료해둠

---
+++ Assets/VideoPlayer/VideoController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoController : MonoBehaviour
+{
+    [SerializeField] RawImage m_rawImage = null;
+    [SerializeField] VideoPlayer videoPlayer = null;
+    [SerializeField] Slider m_sliderTimeScrub = null;
+
+    public string RightScene;
+    public string LeftScene;
+    public UnityEngine.UI.Text GestureStatus;
+
+    void Start() {
+        m_sliderTimeScrub.maxValue = videoPlayer.frameCount;
+        StartCoroutine(PlayVideoCor());
+    }
+
+    private void Update() {
+
+        // 비디오 진행 슬라이더
+        if (videoPlayer.isPlaying == true)
+            m_sliderTimeScrub.value = (float)videoPlayer.frame;
+
+        // 현재 재스쳐를 인식하여 비디오 진행, 정지 결정
+        if (GestureStatus.text.Contains("Run"))
+        {
+            videoPlayer.Play();
+        } else
+        {
+            videoPlayer.Pause();
+        }
+
+        // 갈림길에 도달했을 때 플레이어의 방향전환에 따라 씬 변환
+        if (m_sliderTimeScrub.maxValue * 0.99 <  m_sliderTimeScrub.value)
+        {
+            videoPlayer.Pause();
+            if (GestureStatus.text.Contains("Left"))
+            {
+                SceneManager.LoadScene(LeftScene);
+            } else if (GestureStatus.text.Contains("Right"))
+            {
+                SceneManager.LoadScene(RightScene);
+            }
+        }
+    }
+
+    IEnumerator PlayVideoCor() {
+        videoPlayer.Prepare();
+
+        while (!videoPlayer.isPrepared) {
+            yield return null;
+        }
+
+        m_rawImage.texture = videoPlayer.texture;
+    }
+
+    public void ChageValue() {
+        videoPlayer.frame = (long)m_sliderTimeScrub.value;
+    }
+}
