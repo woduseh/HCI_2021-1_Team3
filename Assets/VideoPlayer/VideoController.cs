96e90ff01d0948959b68f41f548f1daab1fd0997
woduseh <39576391+woduseh@users.noreply.github.com>
2021-04-28 21:06:49 +0900

    동영상 종료시 화살표 추가
    학교입구 씬에만 적용

--- Assets/VideoPlayer/VideoController.cs
+++ Assets/VideoPlayer/VideoController.cs
@@ -13,38 +13,47 @@ public class VideoController : MonoBehaviour
 
     public string RightScene;
     public string LeftScene;
+    public GameObject RightArrow;
+    public GameObject LeftArrow;
     public UnityEngine.UI.Text GestureStatus;
 
     void Start() {
         m_sliderTimeScrub.maxValue = videoPlayer.frameCount;
         StartCoroutine(PlayVideoCor());
+
+        LeftArrow.SetActive(false);
+        RightArrow.SetActive(false);
     }
 
     private void Update() {
-
+        
         // 비디오 진행 슬라이더
         if (videoPlayer.isPlaying == true)
             m_sliderTimeScrub.value = (float)videoPlayer.frame;
-
-        // 현재 재스쳐를 인식하여 비디오 진행, 정지 결정
-        if (GestureStatus.text.Contains("Run"))
-        {
-            videoPlayer.Play();
-        } else
-        {
-            videoPlayer.Pause();
-        }
 
         // 갈림길에 도달했을 때 플레이어의 방향전환에 따라 씬 변환
         if (m_sliderTimeScrub.maxValue * 0.99 <  m_sliderTimeScrub.value)
         {
             videoPlayer.Pause();
+            LeftArrow.SetActive(true);
+            RightArrow.SetActive(true);
             if (GestureStatus.text.Contains("Left"))
             {
                 SceneManager.LoadScene(LeftScene);
             } else if (GestureStatus.text.Contains("Right"))
             {
                 SceneManager.LoadScene(RightScene);
+            }
+        } else
+        {
+            // 현재 재스쳐를 인식하여 비디오 진행, 정지 결정
+            if (GestureStatus.text.Contains("Run"))
+            {
+                videoPlayer.Play();
+            }
+            else
+            {
+                videoPlayer.Pause();
             }
         }
     }
